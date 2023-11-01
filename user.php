98ce87e47d94131596565cfa47d4eff0dcc961c6
hasanrpl14 <4bdisalam@gmail.com>
2023-11-01 21:43:28 +0700

    menambahkan tampilan data user dari databas

--- user.php
+++ user.php
@@ -1,13 +1,156 @@
+<?php 
+    include "proses/koneksi.php";
+    $query = mysqli_query($conn, "SELECT * FROM user"); 
+    // untuk mengambill data dari database from user dari tabel user dan $conn variabel dari file koneksi php
+    while ($record = mysqli_fetch_array($query)) { 
+    // setiap kali iterasi loop while, data dari baris saat ini akan disimpan dalam array $result
+        $result[] = $record;
+    }
+?>
+
 <div class="col-lg-9 mt-2">
-                <div class="card">
-                    <div class="card-header">
-                        User
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eligendi veniam fugit facere recusandae doloribus odio tempora reiciendis sint mollitia sit animi distinctio libero,
-                             eum nemo non vitae est ea?</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="card">
+        <div class="card-header">
+            User
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col d-flex justify-content-end">
+                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#TambahUser"> Tambah User</button>
+                </div>
+            </div>
+              <!-- Modal Tambah User-->
+            <div class="modal fade" id="TambahUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-xl modal-fullscreen-md-down">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah User</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
                     </div>
                 </div>
             </div>
+               <!--Akhir Modal Tmbah User-->
+
+            <!-- Modal Button View-->
+            <div class="modal fade" id="ModalView" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-xl modal-fullscreen-md-down">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">View User</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+             <!--Akhir Modal Button View-->
+
+            <!-- Modal Button Edit-->
+            <div class="modal fade" id="EditUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-xl modal-fullscreen-md-down">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Edit User</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+             <!--Akhir Modal Button Edit-->
+
+            <!-- Modal Button Hapus-->
+            <div class="modal fade" id="HapusUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-xl modal-fullscreen-md-down">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Hapus User</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+             <!--Akhir Modal Button Edit-->
+
+             <?php 
+                if (empty($result)){
+                    echo "Data User tidak ada";
+                }else{
+
+             ?>
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Level</th>
+                        <th scope="col">No Hp</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $no = 1;
+                            foreach ($result as $row) {
+                            ?>
+                        <tr>
+                            <th scope="row"><?php echo $no++ ?></th>
+                            <td>
+                                <?php echo $row['nama'] ?>
+                            </td>
+                            <td>
+                                <?php echo $row['username'] ?>
+                            </td>
+                            <td>
+                                <?php echo $row['id_level'] ?>
+                            </td>
+                            <td>@mdo</td>
+                            <td class="d-flex">
+                                <button class="btn btn-info btn-sm me-1" data-bs-toggle="modal" data-bs-target="#ModalView"> 
+                                    <i class="bi bi-eye"></i></button>
+                                <button class="btn btn-warning btn-sm me-1" data-bs-toggle="modal" data-bs-target="#EditUser"> 
+                                    <i class="bi bi-pencil-square"></i></button>
+                                <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#HapusUser"> 
+                                    <i class="bi bi-trash"></i></button>
+                            </td>
+                            <?php
+                            }
+                            ?>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <?php
+             }
+            ?>
+        </div>
+    </div>
+</div>
